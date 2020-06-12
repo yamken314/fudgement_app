16f2c12a0d7f34d4226a856c7d75bf73aad0d35d
yamamoto <yamamoto314@gmail.com>
2020-06-13 04:55:27 +0900

    ログイン画面、エラー表示実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
+    flash.now[:danger] = 'メール/パスワードの組み合わせが無効です'
     render 'new'
     end
   end
