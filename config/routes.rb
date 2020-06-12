11d44270f2a4239ea330957a77ab7a5448333bb1
yamamoto <yamamoto314@gmail.com>
2020-06-13 04:45:55 +0900

    ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   # get 'users/new'
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   resources :users
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
