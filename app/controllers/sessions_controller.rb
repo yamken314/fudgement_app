cf2ca59e11cba909bd700f9044392854558a6390
yamamoto <yamamoto314@gmail.com>
2020-06-13 21:42:42 +0900

    log_in crrent_user実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
     flash.now[:danger] = 'メール/パスワードの組み合わせが無効です'
     render 'new'
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 end
