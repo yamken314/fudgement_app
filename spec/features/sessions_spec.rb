02cf6b9246aa1f7984f7b3e753ae702714aae4c2
yamamoto <yamamoto314@gmail.com>
2020-06-15 10:26:07 +0900

    ログアウトテスト

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.feature "Sessions", type: :feature do
   scenario "無効な情報にログイン" do
-    user = FactoryBot.create(:user)
     visit login_path
     fill_in "Email", with: ""
     fill_in "パスワード", with: ""
     click_button "ログイン" 
 
     expect(page).to have_content "メール/パスワードの組み合わせが無効です"
+  end
+
+  scenario "有効な情報にログインしてログアウト" do
+    user = FactoryBot.create(:user)
+    visit login_path
+    fill_in "Email", with: "yamamoto@email.com"
+    fill_in "パスワード", with: "yamamoto"
+    click_button "ログイン" 
+    
+    expect(page).to have_content "#{user.name}"
+
+    click_link "ログアウト"
+    expect(page).to_not have_content "ログアウト"
   end
 end
