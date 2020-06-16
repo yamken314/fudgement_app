8ba298b9bc8fad379349af5bb760ad9805f9d8a5
yamamoto <yamamoto314@gmail.com>
2020-06-16 22:29:39 +0900

    異なるブラウザで連続ログアウトするとエラー修正

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "UsersSignups", type: :request do
-  example "有効な情報でサインアップ" do
+  example "有効な情報でサインアップして連続ログアウト" do
     get signup_path
     expect{
       post users_path, params: { user: { name:  "Example User",
@@ -13,5 +13,18 @@ RSpec.describe "UsersSignups", type: :request do
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
+
+    expect{
+      delete login_path, params: { user: { name:  "Example User",
+        email: "user@example.com",
+        password:              "password",
+        password_confirmation: "password" }}
+      }.to change(User, :count).by(0)
+    expect{
+      delete login_path, params: { user: { name:  "Example User",
+        email: "user@example.com",
+        password:              "password",
+        password_confirmation: "password" }}
+      }.to change(User, :count).by(0)
   end
 end
