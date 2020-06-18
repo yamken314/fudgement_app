aa1bf29f74aee6444927339588727d2d9a4aef02
yamamoto <yamamoto314@gmail.com>
2020-06-18 11:35:32 +0900

    自動ログインテスト

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "UsersSignups", type: :request do
+  let(:user){FactoryBot.create(:user)}
+
   example "有効な情報でサインアップして連続ログアウト" do
     get signup_path
     expect{
@@ -26,5 +28,23 @@ RSpec.describe "UsersSignups", type: :request do
         password:              "password",
         password_confirmation: "password" }}
       }.to change(User, :count).by(0)
+  end
+
+  context "自動ログインテスト" do
+    it "自動ログインを選択" do
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password,
+                                            remember_me: '1'}}
+      expect(cookies['remember_token']).to_not eq nil
+      expect(assigns(:user).remember_token).to_not eq nil
+    end
+
+    it "自動ログインを選択しない" do
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password,
+                                            remember_me: '0'}}
+      expect(cookies['remember_token']).to eq nil
+      expect(assigns(:user).remember_token).to eq nil
+    end
   end
 end
