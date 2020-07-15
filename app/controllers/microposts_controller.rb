303841fdd4bc9703c349a6d66d242c730ade54bf
山本健輔 <yamamoto314@gmail.com>
2020-07-16 06:43:52 +0900

    コメント投稿機能実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,27 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only:[:create,:destroy]
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "コメントを作成しました"
+      redirect_to root_url
+    else
+      render 'microposts/new'
+    end
+  end
+
+  def destroy
+  end
+
+  def new
+    @micropost = current_user.microposts.build if logged_in?
+    
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -52,13 +53,6 @@ class UsersController < ApplicationController
   
       def user_params
         params.require(:user).permit(:name,:email,:password,:password_confirmation)
-      end
-
-      def logged_in_user
-        unless logged_in?
-          flash[:danger] = "ログインして下さい"
-          redirect_to login_url
-        end
       end
 
       def currect_user
