8b3e14fcc5eef091792c772cb062f9c707e1c4bb
山本健輔 <yamamoto314@gmail.com>
2020-08-09 14:54:11 +0900

    gem pry導入、テスト(保留)修正分 #8

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  let(:user) { create(:user) }
-  let(:other_user) { create(:other_user) }
-  before { @user = user.follow(other_user) }
+  let(:relationship) { build(:relationship) }
   it "Relationshipは有効" do
-    @user.valid?
-    expect(@user).to be_valid
+    relationship.create
+    relationship.valid?
+    expect(relationship).to be_valid
   end
 
   it "Relarionshipはfollower_idが必要" do
-    @user.follower_id = nil
-    @user.valid?
-    expect(@user).to_not be_valid
+    relationship.follower_id = nil
+    relationship.valid?
+    expect(relationship).to_not be_valid
   end
 
   it "Relationshipはfollowed_idが必要" do
-    @user.followed_id = nil
-    @user.valid?
-    expect(@user).to_not be_valid
+    relationship.followed_id = nil
+    relationship.valid?
+    expect(relationship).to_not be_valid
   end
 end
