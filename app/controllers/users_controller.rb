2ac606b0510115e1578e5ac73329147a98aef1dd
山本健輔 <yamamoto314@gmail.com>
2020-08-23 00:54:47 +0900

    following followersメソッド、DRY #15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :currect_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :set_users, only: [:following, :followers]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -47,19 +48,18 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  def following
-    @user = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
-  end
+  def following; end
 
-  def followers
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
-  end
+  def followers; end
 
   private
+
+  def set_users
+    @user = User.find(params[:id])
+    users = action_name == 'following' ? @user.following : @user.followers
+    @users = users.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
