f53f32f713bf0eaff0acdbc4678d04d0a9baec93
山本健輔 <yamamoto314@gmail.com>
2020-08-27 03:13:08 +0900

    voteコントローラーにrespond_to、_choice.html.erbパーシャル追加 #23

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,20 @@
 class VotesController < ApplicationController
   def create
     vote = current_user.votes.build(choice_id: params[:choice_id])
+    @choice = Choice.find(params[:choice_id])
     vote.save
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
   def destroy
     vote = Vote.find_by(choice_id: params[:choice_id], user_id: current_user.id)
     vote.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 end
