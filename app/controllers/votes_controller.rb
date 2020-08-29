8b7d3301f8469809c18361537285aaa86e585515
山本健輔 <yamamoto314@gmail.com>
2020-08-29 11:47:46 +0900

    votesコントローラー変数名、変更。 #23

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
   def create
     vote = current_user.votes.build(choice_id: params[:choice_id])
     @choice = Choice.find(params[:choice_id])
-    @vote = Micropost.find(@choice.micropost.id)
+    @micropost = Micropost.find(@choice.micropost.id)
     vote.save
     respond_to do |format|
       format.html { redirect_to root_path }
@@ -13,7 +13,7 @@ class VotesController < ApplicationController
   def destroy
     vote = Vote.find_by(choice_id: params[:choice_id], user_id: current_user.id)
     @choice = Choice.find(params[:choice_id])
-    @vote = Micropost.find(@choice.micropost.id)
+    @micropost = Micropost.find(@choice.micropost.id)
     vote.destroy
     respond_to do |format|
       format.html { redirect_to root_path }
