8bf530f96eb65ac6bc7d625089044825412af743
山本健輔 <yamamoto314@gmail.com>
2020-09-06 23:41:00 +0900

    redirect先、変更。 #23

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,9 @@ class VotesController < ApplicationController
   def create
     vote = current_user.votes.build(choice_id: params[:choice_id])
     @choice = Choice.find(params[:choice_id])
-    @micropost = Micropost.find(@choice.micropost.id)
     vote.save
     respond_to do |format|
-      format.html { redirect_to root_path }
+      format.html { redirect_back(fallback_location: root_path) }
       format.js
     end
   end
@@ -13,10 +12,9 @@ class VotesController < ApplicationController
   def destroy
     vote = Vote.find_by(choice_id: params[:choice_id], user_id: current_user.id)
     @choice = Choice.find(params[:choice_id])
-    @micropost = Micropost.find(@choice.micropost.id)
     vote.destroy
     respond_to do |format|
-      format.html { redirect_to root_path }
+      format.html { redirect_back(fallback_location: root_path) }
       format.js
     end
   end
