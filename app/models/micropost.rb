a2a8ee0c9236dc5640e9d50e5ae52994e5422d6b
山本健輔 <yamamoto314@gmail.com>
2020-09-15 01:28:48 +0900

    micropostモデル、フォーム削除のallow_destroy追加。　#45

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :choices
-  accepts_nested_attributes_for :choices
   scope :sorted_desc, -> { order(created_at: :desc) }
   has_many_attached :images
   validates :content, presence: true, length: { maximum: 140 }
+  accepts_nested_attributes_for :choices, allow_destroy: true
 end
