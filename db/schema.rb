41d10b9d446cff5470aab959ccf43ee3b13d7d0c
山本健輔 <yamamoto314@gmail.com>
2020-09-18 20:16:31 +0900

    Choicesモデル、user_id外部キー追加。#61

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_170245) do
+ActiveRecord::Schema.define(version: 2020_09_18_111039) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2020_08_09_170245) do
     t.bigint "micropost_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["micropost_id"], name: "index_choices_on_micropost_id"
+    t.index ["user_id"], name: "index_choices_on_user_id"
   end
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -83,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_170245) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "choices", "microposts"
+  add_foreign_key "choices", "users"
   add_foreign_key "microposts", "users"
   add_foreign_key "relationships", "users", column: "followed_id"
   add_foreign_key "relationships", "users", column: "follower_id"
