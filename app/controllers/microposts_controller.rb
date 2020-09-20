1dcc9e0046695706c9101c916711547e137a92ff
山本健輔 <yamamoto314@gmail.com>
2020-09-21 00:19:54 +0900

    joinsを消して、eager_loadを定義。 #65

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -48,7 +48,7 @@ class MicropostsController < ApplicationController
 
     def set_micropost
       @micropost = Micropost.with_attached_images.find(params[:id])
-      @choice = @micropost.choices.joins(:user)
+      @choice = @micropost.choices.eager_load(:user)
     end
 
     def set_colors
