fad62d729bd320cf8cc9c738d3e0810a07f6ee02
山本健輔 <yamamoto314@gmail.com>
2020-09-23 03:03:49 +0900

    choiceモデルバリデーション追加。 #78

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,5 +2,6 @@ class Choice < ApplicationRecord
   belongs_to :micropost
   has_many :votes, dependent: :destroy
   validates_presence_of :micropost
+  validates :name, presence: true, length: { maximum: 140 }
   belongs_to :user
 end
