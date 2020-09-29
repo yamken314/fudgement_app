ce0aef5831ebecbcf5eee055f7adc66379472cdb
山本健輔 <yamamoto314@gmail.com>
2020-09-29 20:24:44 +0900

    N+1解消。 #96

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @microposts = Micropost.all.includes(:user, :choices).with_attached_images.sorted_desc
+    @microposts = Micropost.all.includes(:choices, user: {avatar_attachment: :blob}).with_attached_images.sorted_desc
     @colors = ['post_details_1','post_details_2', 'post_details_3', 'post_details_4']
   end
 
