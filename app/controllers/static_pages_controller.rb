b762304777f9f20c6ed18fe91510a3a384ee5806
山本健輔 <yamamoto314@gmail.com>
2020-10-06 15:55:02 +0900

    コントローラーにページネーション追加。 #117

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @microposts = Micropost.all.eager_load(user: {avatar_attachment: :blob}).with_attached_image.sorted_desc
+    @microposts = Micropost.all.eager_load(user: {avatar_attachment: :blob}).with_attached_image.sorted_desc.page(params[:page])
     @colors = ['post_details_1','post_details_2', 'post_details_3', 'post_details_4']
   end
 
