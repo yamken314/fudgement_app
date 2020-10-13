2e5fa46e8349648fe62bc8b84cd51cb5cacdbf8d
山本健輔 <yamamoto314@gmail.com>
2020-10-13 21:06:14 +0900

    image画像、オリジナルサイズ取得。　#132

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'],
+  :image_size => 'original'
 end
