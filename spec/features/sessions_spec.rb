a2cc4074c44baaec68d842ed570dcc281f456c6e
山本健輔 <yamamoto314@gmail.com>
2020-10-24 17:19:36 +0900

    ゲストログインテスト追加。　#165

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -6,20 +6,23 @@ RSpec.feature "Sessions", type: :feature do
     fill_in "Email", with: ""
     fill_in "パスワード", with: ""
     click_button "ログイン" 
-
     expect(page).to have_content "メール/パスワードの組み合わせが無効です"
   end
 
   scenario "有効な情報にログインしてログアウト" do
-    user = FactoryBot.create(:user)
+    FactoryBot.create(:user)
     visit login_path
     fill_in "Email", with: "yamamoto@email.com"
     fill_in "パスワード", with: "yamamoto"
     click_button "ログイン" 
-    
     expect(page).to have_content "yamamoto"
+    click_link "Log out"
+    expect(page).to_not have_content "Log out"
+  end
 
-    click_link "ログアウト"
-    expect(page).to_not have_content "ログアウト"
+  scenario "ゲストログインでログインする" do
+    visit login_path
+    click_link "ゲストログイン"
+    expect(page).to have_content "ゲストログインしました。"
   end
 end
