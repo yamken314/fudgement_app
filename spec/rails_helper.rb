8d6909b3581d8686310dfd139e3568a92bb5a4ed
山本健輔 <yamamoto314@gmail.com>
2020-10-26 01:36:13 +0900

    rails_helper設定。 #168

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+OmniAuth.config.test_mode = true
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -57,6 +57,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   require 'capybara/rspec'
   config.include IntegretionHelpers, type: :request
-  config.include IntegretionHelpers, type: :feature
+  config.include IntegretionHelpers, type: :system
   config.include FactoryBot::Syntax::Methods
 end
