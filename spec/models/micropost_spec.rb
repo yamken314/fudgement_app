a8520448807d34160653bcc30138d417be082447
山本健輔 <yamamoto314@gmail.com>
2020-10-29 15:12:37 +0900

    micropostsモデルスペック修正。#177

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,28 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-
-  before do
-    @user = FactoryBot.create(:user)
-    @micropost = @user.microposts.create(content: "おはようございます")
-  end
+  let(:user) { create(:user) }
+  let(:micropost) { Micropost.new(content: "おはようございます", user_id: user.id) }
   
   it "Micropostは有効か"do
-    expect(@micropost).to be_valid
+    expect(micropost).to be_valid
   end
 
   it "Micropostはuser_idが必要"do
-    @micropost.user_id = nil
-    expect(@micropost).to_not be_valid
+    micropost.user_id = nil
+    micropost.valid?
+    expect(micropost).to_not be_valid
   end
 
   it "contentの空白は無効" do
-    @micropost.content = ""
-    expect(@micropost).to_not be_valid
+    micropost.content = ""
+    expect(micropost).to_not be_valid
   end
 
   it "contentの文字数が140文字以上だったら無効" do
-    @micropost.content = "a" * 141
-    expect(@micropost).to_not be_valid
+    micropost.content = "a" * 141
+    expect(micropost).to_not be_valid
+  end
+
+  it "contentの文字数が140字以内だったら有効" do
+    micropost.content = "a" * 140
+    expect(micropost).to be_valid
   end
 end
