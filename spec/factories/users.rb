363af735651d68d10feeba6e5b160b7b45a4741a
山本健輔 <yamamoto314@gmail.com>
2020-11-05 11:20:54 +0900

    トレイト作成。 #191

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,4 +11,34 @@ FactoryBot.define do
     email { "tanaka@email.com"}
     password { "tanaka"}
   end
+
+  trait :with_avatar do
+    avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test_image.jpg'))
+    }
+  end
+
+  trait :invalid_image_avater do
+    avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', '10megabytes.jpg'))
+    }
+  end
+
+  trait :invalid_file_avatar do
+    avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'invalid_file.pdf'))
+    }
+  end
+
+  trait :with_profile_image do
+    profile_image { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test_image.jpg'))
+    }
+  end
+
+  trait :invalid_image_profile_image do
+    profile_image { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', '10megabytes.jpg'))
+    }
+  end
+
+  trait :invalid_file_profile_image do
+    profile_image { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'invalid_file.pdf'))
+    }
+  end
 end
