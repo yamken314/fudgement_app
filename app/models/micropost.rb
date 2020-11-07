e3d8273ca1a2c6c8c86a69b56b3954cce200d9fc
山本健輔 <yamamoto314@gmail.com>
2020-11-07 17:53:31 +0900

    プロフィール画像バリデーション作成。 #193

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,23 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   validates :content, presence: true, length: { maximum: 140 }
   accepts_nested_attributes_for :choices, allow_destroy: true
+  validate  :validate_image
+
+  def validate_image
+    return unless image.attached?
+
+    if image.blob.byte_size > 10.megabytes
+      image.purge
+      errors.add(:image, 'ファイルのサイズが大きすぎます(10MB以内)')
+    elsif !image?
+      image.purge
+      errors.add(:image, 'ファイルが対応している画像データではありません')
+    end
+  end
+
+  private
+
+  def image?
+    %w[image/jpg image/jpeg image/gif image/png].include?(image.blob.content_type)
+  end
 end
